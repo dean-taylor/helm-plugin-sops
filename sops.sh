27cbbb51f94c909836821c0fead320be6c2fedb8
Dean Taylor <Dean.Taylor@uwa.edu.au>
2021-09-06 19:09:05 +0800

    initial commit

---
+++ sops.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin'
+
+TMP_DIR=$(mktemp -d)
+function cleanup () {
+	rm -rf "${TMP_DIR}"
+}
+
+TEMP=$(getopt -o 'hf:' --long 'help,values:' -- "$@")
+[ $? -ne 0 ] && { >&2 echo 'Terminating...'; exit 128; }
+
+eval set -- "$TEMP"
+unset TEMP
+
+trap cleanup EXIT
+
+VALUES=()
+while true; do
+	case "$1" in
+		'-f'|'--values')
+			if grep -q '^sops:$' "${2}"; then
+				TMP_FILE=$(mktemp -u $TMP_DIR/tmp.XXXXXXXXXX)
+				(umask 0077; mkfifo "${TMP_FILE}") 
+				VALUES+=("${TMP_FILE}")
+				sops -d "${2}" > "${TMP_FILE}" &
+			else
+				VALUES+=("${2}")
+			fi
+			shift 2
+			;;
+		'-h'|'--help')
+			echo 'HELP'
+			shift
+			;;
+		'--')
+			shift
+			break
+			;;
+		*)
+			>&2 echo 'Internal error!'
+			exit 128
+			;;
+	esac
+done
+
+VALUES_STR=""
+for f in "${VALUES[@]}"; do
+	VALUES_STR+=" --values ${f}"
+done
+
+[[ $1 =~ ^install|template|upgrade$ ]] && $HELM_BIN $@ $VALUES_STR --post-renderer kustomize-wrapper.sh || $HELM_BIN $@ $VALUES_STR
